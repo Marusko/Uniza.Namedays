429b11f1c6e881a44bf9bf153c72a3ff8284763e
Matúš Suský <matussusky@gmail.com>
2023-05-04 15:31:21 +0200

    Add project files.

---
+++ Uniza.Namedays/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Uniza.Namedays
+{
+    internal class Program
+    {
+    }
+
+    struct MyStruct
+    {
+        public int Day { get; init; }
+        public int Month { get; init; }
+    }
+}
