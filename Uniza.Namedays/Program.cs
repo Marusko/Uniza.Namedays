92e96e752cca0ce839c1e414efced2cfc45368b8
Matúš Suský <matussusky@gmail.com>
2023-05-04 15:35:13 +0200

    Dokoncena DayMonth

--- Uniza.Namedays/Program.cs
+++ Uniza.Namedays/Program.cs
@@ -4,9 +4,26 @@
     {
     }
 
-    struct MyStruct
+    struct DayMonth
     {
         public int Day { get; init; }
         public int Month { get; init; }
+
+        public DayMonth()
+        {
+            Day = DateTime.Now.Day;
+            Month = DateTime.Now.Month;
+        }
+
+        public DayMonth(int day, int month)
+        {
+            Day = day;
+            Month = month;
+        }
+
+        public DateTime ToDateTime()
+        {
+            return new DateTime(DateTime.Now.Year, Month, Day);
+        }
     }
 }
