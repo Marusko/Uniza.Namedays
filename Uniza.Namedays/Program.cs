d0f5532553960c0e9366ea49e1158339a700ad67
Matúš Suský <matussusky@gmail.com>
2023-05-10 11:27:05 +0200

    NamedayCalendar added methods

--- Uniza.Namedays/Program.cs
+++ Uniza.Namedays/Program.cs
@@ -4,14 +4,76 @@ namespace Uniza.Namedays
 {
     record NamedayCalendar : IEnumerable<Nameday>
     {
+        public int NameCount { get; }
+        public int DayCount { get; }
+        public DayMonth? this[string name] { get; }
+        public string[] this[DayMonth dayMonth] { get; }
+        public string[] this[DateOnly date] { get; }
+        public string[] this[DateTime date] { get; }
+        public string[] this[int day, int month] { get; }
+
         public IEnumerator<Nameday> GetEnumerator()
         {
             throw new NotImplementedException();
         }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+
+        public IEnumerable<Nameday> GetNamedays()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Nameday> GetNamedays(int month)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Nameday> GetNamedays(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Nameday nameday)
+        {
+
+        }
+
+        public void Add(int day, int month, params string[] names)
+        {
+
+        }
+
+        public void Add(DayMonth dayMonth, params string[] names)
+        {
+
+        }
+
+        public bool Remove(string name)
+        {
+
+        }
+
+        public bool Contains(string name)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public void Load(FileInfo csvFile)
+        {
+
+        }
+
+        public void Save(FileInfo csvFile)
+        {
+
         }
     }
 
