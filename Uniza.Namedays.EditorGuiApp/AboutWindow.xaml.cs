0a2e1b5d9f1ae038e4e2bbc76cd4e3e4d2e966b9
Matúš Suský <matussusky@gmail.com>
2023-05-15 11:21:27 +0200

    Fungujuce menu, pridane okno o aplikacii

---
+++ Uniza.Namedays.EditorGuiApp/AboutWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace Uniza.Namedays.EditorGuiApp
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CloseWindow(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- Uniza.Namedays.EditorGuiApp/MainWindow.xaml.cs
+++ Uniza.Namedays.EditorGuiApp/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Uniza.Namedays.EditorGuiApp
 {
@@ -20,9 +11,46 @@ namespace Uniza.Namedays.EditorGuiApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NamedayCalendar _calendar;
         public MainWindow()
         {
             InitializeComponent();
+            _calendar = new NamedayCalendar();
+        }
+
+        public void MenuNew(object sender, EventArgs e)
+        {
+            if (_calendar.GetNamedays().ToList().Count > 0)
+            {
+                var dialog = MessageBox.Show("Do you realy want to clear the calendar?", "New", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (dialog == MessageBoxResult.Yes)
+                {
+                    _calendar.Clear();
+                }
+            }
+        }
+        public void MenuOpen(object sender, EventArgs e)
+        {
+            var result = new OpenFileDialog();
+            result.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            result.ShowDialog();
+            _calendar.Load(new FileInfo(result.FileName));
+        }
+        public void MenuSave(object sender, EventArgs e)
+        {
+            var save = new SaveFileDialog();
+            save.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            save.ShowDialog();
+            _calendar.Save(new FileInfo(save.FileName));
+        }
+        public void MenuExit(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        public void MenuAbout(object sender, EventArgs e)
+        {
+            var about = new AboutWindow();
+            about.ShowDialog();
         }
     }
 }
